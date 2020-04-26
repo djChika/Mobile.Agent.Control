897b97d28d7321c915d5736eadcd1ed4a2d04886
Chika <djchikalkin@gmail.com>
2020-04-26 19:24:33 +0300

    remove routes from server app

--- server/index.js
+++ server/index.js
@@ -1,24 +1,14 @@
 const express = require('express');
 const app = express();
 const path = require('path');
-var bodyParser = require('body-parser');
 
 const PORT = 3005;
 
-app.use(bodyParser.json());
-
-// frontapp
 app.use(express.static('build'));
 
 app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname + '/build', '/index.html'));
 });
-// frontapp
-
-//routes
-var newsRouter = require('./routes/news');
-app.use(newsRouter);
-//routes
 
 app.listen(process.env.port || PORT);
 
